8dadc8af8d5a076b7e20b175e9f772de44003f03
unilock <unilock@fennet.rentals>
2024-11-06 08:20:30 -0500

    init

---
+++ src/main/java/cc/unilock/unipack/UniPack.java
@@ -0,0 +1,21 @@
+package cc.unilock.unipack;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+@Mod(UniPack.MOD_ID)
+public class UniPack {
+    public static final String MOD_ID = "unipack";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public UniPack(IEventBus modEventBus) {
+        modEventBus.addListener(this::fmlCommonSetup);
+    }
+
+    private void fmlCommonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("UNILOCK WAS HERE");
+    }
+}
