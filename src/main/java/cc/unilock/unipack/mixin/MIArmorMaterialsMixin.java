d99aa5b84a4a1b240c70c486bf3c8dd0c407c8b8
unilock <unilock@fennet.rentals>
2024-12-13 20:46:06 -0500

    torture
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/unipack/mixin/MIArmorMaterialsMixin.java
@@ -0,0 +1,26 @@
+package cc.unilock.unipack.mixin;
+
+import aztech.modern_industrialization.items.armor.MIArmorMaterials;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = MIArmorMaterials.class, remap = false)
+public class MIArmorMaterialsMixin {
+	@Inject(method = "lambda$static$0", at = @At("HEAD"), cancellable = true)
+	private static void dieselJetpack(CallbackInfoReturnable<Ingredient> cir) {
+		cir.setReturnValue(Ingredient.EMPTY);
+	}
+
+	@Inject(method = "lambda$static$2", at = @At("HEAD"), cancellable = true)
+	private static void graviChestPlate(CallbackInfoReturnable<Ingredient> cir) {
+		cir.setReturnValue(Ingredient.EMPTY);
+	}
+
+	@Inject(method = "lambda$static$7", at = @At("HEAD"), cancellable = true)
+	private static void quantumArmor(CallbackInfoReturnable<Ingredient> cir) {
+		cir.setReturnValue(Ingredient.EMPTY);
+	}
+}
