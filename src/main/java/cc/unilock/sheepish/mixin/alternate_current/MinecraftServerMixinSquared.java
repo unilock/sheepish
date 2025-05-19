44f817a795d6df5517fbe01885296bdc9791ffc3
unilock <unilock@fennet.rentals>
2025-05-19 07:00:02 -0400

    alternate_current npe fix
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/sheepish/mixin/alternate_current/MinecraftServerMixinSquared.java
@@ -0,0 +1,26 @@
+package cc.unilock.sheepish.mixin.alternate_current;
+
+import com.bawnorton.mixinsquared.TargetHandler;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = MinecraftServer.class, priority = 1500)
+public abstract class MinecraftServerMixinSquared {
+	@Shadow public abstract ServerLevel overworld();
+
+	@TargetHandler(
+			mixin = "alternate.current.mixin.MinecraftServerMixin",
+			name = "alternate_current$save(ZZZLorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;)V"
+	)
+	@Inject(method = "@MixinSquared:Handler", at = @At("HEAD"))
+	private void save(CallbackInfo ci) {
+		if (this.overworld() == null) {
+			ci.cancel();
+		}
+	}
+}
