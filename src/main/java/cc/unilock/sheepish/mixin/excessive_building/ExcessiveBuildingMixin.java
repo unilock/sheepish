c4e00876cc19572005f12957beaeaf860b50c4f8
unilock <unilock@fennet.rentals>
2025-05-24 04:49:11 -0400

    fix targets strings
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/sheepish/mixin/excessive_building/ExcessiveBuildingMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/excessive_building/ExcessiveBuildingMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "net.yirmiri.excessive_building.ExcessiveBuilding")
+@Mixin(targets = "net/yirmiri/excessive_building/ExcessiveBuilding")
 @Pseudo
 public class ExcessiveBuildingMixin {
 	@Redirect(method = "onInitialize", at = @At(value = "INVOKE", target = "Lfuzs/forgeconfigapiport/fabric/api/forge/v4/ForgeConfigRegistry;register(Ljava/lang/String;Lnet/neoforged/fml/config/ModConfig$Type;Lnet/minecraftforge/fml/config/IConfigSpec;Ljava/lang/String;)V"))
--- src/main/java/cc/unilock/sheepish/mixin/minecraft/TreasureMapForEmeraldsMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/minecraft/TreasureMapForEmeraldsMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "net.minecraft.world.entity.npc.VillagerTrades$TreasureMapForEmeralds")
+@Mixin(targets = "net/minecraft/world/entity/npc/VillagerTrades$TreasureMapForEmeralds")
 public class TreasureMapForEmeraldsMixin {
 	@Redirect(method = "getOffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;findNearestMapStructure(Lnet/minecraft/tags/TagKey;Lnet/minecraft/core/BlockPos;IZ)Lnet/minecraft/core/BlockPos;"))
 	private BlockPos getOffer$findNearestMapStructure(ServerLevel instance, TagKey<Structure> optional, BlockPos blockPos, int structureTag, boolean pos) {
