9904d8f9ea6c5f1a2cecba310fe43a9fe76b60a8
unilock <unilock@fennet.rentals>
2025-05-30 05:50:56 -0400

    fix akashic tome transform
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/sheepish/Sheepish.java
+++ src/main/java/cc/unilock/sheepish/Sheepish.java
@@ -3,20 +3,25 @@ package cc.unilock.sheepish;
 import com.almostreliable.unified.api.AlmostUnified;
 import com.mojang.logging.LogUtils;
 import fuzs.forgeconfigapiport.neoforge.api.forge.v4.ForgeConfigRegistry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.loading.LoadingModList;
+import net.neoforged.neoforge.client.event.InputEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockDropsEvent;
 import org.slf4j.Logger;
+import vazkii.akashictome.MorphingHandler;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Collection;
@@ -26,11 +31,19 @@ public class Sheepish {
     public static final String MOD_ID = "sheepish";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    private static final boolean AKASHICTOME = LoadingModList.get().getModFileById("akashictome") != null;
     private static final boolean ALMOSTUNIFIED = LoadingModList.get().getModFileById("almostunified") != null;
     private static final boolean ANSHAR = LoadingModList.get().getModFileById("anshar") != null;
     private static final boolean EXCESSIVE_BUILDING = LoadingModList.get().getModFileById("excessive_building") != null;
 
     public Sheepish() {
+        if (AKASHICTOME) {
+            NeoForge.EVENT_BUS.addListener(InputEvent.InteractionKeyMappingTriggered.class, event -> {
+                if (event.isAttack() && Minecraft.getInstance().hitResult != null && Minecraft.getInstance().hitResult.getType() == HitResult.Type.MISS) {
+                    MorphingHandler.INSTANCE.onPlayerLeftClick(new PlayerInteractEvent.LeftClickEmpty(Minecraft.getInstance().player));
+                }
+            });
+        }
         if (ALMOSTUNIFIED) {
             NeoForge.EVENT_BUS.addListener(EventPriority.HIGHEST, this::blockDrops);
             NeoForge.EVENT_BUS.addListener(EventPriority.HIGHEST, this::livingDrops);
