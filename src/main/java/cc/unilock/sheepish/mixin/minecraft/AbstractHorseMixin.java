7f4c6c99b53025bc331d00b0f07a1962041edaa7
unilock <unilock@fennet.rentals>
2025-06-30 05:45:44 -0400

    tweak Horse Stonks impl
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/sheepish/mixin/minecraft/AbstractHorseMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/minecraft/AbstractHorseMixin.java
@@ -4,18 +4,26 @@ import cc.unilock.sheepish.SheepishConfig;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.level.Level;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AbstractHorse.class)
-public class AbstractHorseMixin {
+public abstract class AbstractHorseMixin extends LivingEntity {
+	protected AbstractHorseMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+		super(entityType, level);
+		throw new AssertionError();
+	}
+
 	@WrapOperation(method = "setOffspringAttributes", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/animal/horse/AbstractHorse;MIN_HEALTH:F", opcode = Opcodes.GETSTATIC))
 	private float setOffspringAttributes$getMinHealth(Operation<Float> original, AgeableMob parent) {
 		if (SheepishConfig.CONFIG.horseStonks.getAsBoolean()) {
-			return (float) parent.getAttributeBaseValue(Attributes.MAX_HEALTH);
+			return (float) Math.min(this.getAttributeBaseValue(Attributes.MAX_HEALTH), parent.getAttributeBaseValue(Attributes.MAX_HEALTH));
 		} else {
 			return original.call();
 		}
@@ -24,7 +32,7 @@ public class AbstractHorseMixin {
 	@WrapOperation(method = "setOffspringAttributes", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/animal/horse/AbstractHorse;MIN_JUMP_STRENGTH:F", opcode = Opcodes.GETSTATIC))
 	private float setOffspringAttributes$getMinJumpStrength(Operation<Float> original, AgeableMob parent) {
 		if (SheepishConfig.CONFIG.horseStonks.getAsBoolean()) {
-			return (float) parent.getAttributeBaseValue(Attributes.JUMP_STRENGTH);
+			return (float) Math.min(this.getAttributeBaseValue(Attributes.JUMP_STRENGTH), parent.getAttributeBaseValue(Attributes.JUMP_STRENGTH));
 		} else {
 			return original.call();
 		}
@@ -33,7 +41,7 @@ public class AbstractHorseMixin {
 	@WrapOperation(method = "setOffspringAttributes", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/animal/horse/AbstractHorse;MIN_MOVEMENT_SPEED:F", opcode = Opcodes.GETSTATIC))
 	private float setOffspringAttributes$getMinMovementSpeed(Operation<Float> original, AgeableMob parent) {
 		if (SheepishConfig.CONFIG.horseStonks.getAsBoolean()) {
-			return (float) parent.getAttributeBaseValue(Attributes.MOVEMENT_SPEED);
+			return (float) Math.min(this.getAttributeBaseValue(Attributes.MOVEMENT_SPEED), parent.getAttributeBaseValue(Attributes.MOVEMENT_SPEED));
 		} else {
 			return original.call();
 		}
