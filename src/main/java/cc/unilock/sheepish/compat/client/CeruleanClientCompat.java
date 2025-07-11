065b81cd8bfbd36fded3c257d6e0ac8189f9bdb2
unilock <unilock@fennet.rentals>
2025-07-11 07:56:09 -0400

    better fix for Cerulean

--- src/main/java/cc/unilock/sheepish/compat/client/CeruleanClientCompat.java
+++ src/main/java/cc/unilock/sheepish/compat/client/CeruleanClientCompat.java
@@ -1,19 +1,77 @@
 package cc.unilock.sheepish.compat.client;
 
-import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import org.joml.Matrix4f;
 
 public class CeruleanClientCompat {
-	private static final ResourceKey<Level> DREAMSCAPE = ResourceKey.create(Registries.DIMENSION, ResourceLocation.fromNamespaceAndPath("cerulean", "dreamscape"));
-	private static final ResourceKey<Level> SKIES = ResourceKey.create(Registries.DIMENSION, ResourceLocation.fromNamespaceAndPath("cerulean", "skies"));
+	private static final ResourceLocation DREAMSCAPE = ResourceLocation.fromNamespaceAndPath("cerulean", "dreamscape");
+	private static final ResourceLocation SKIES = ResourceLocation.fromNamespaceAndPath("cerulean", "skies");
 
 	public static void init() {
-		DimensionRenderingRegistry.registerCloudRenderer(DREAMSCAPE, ctx -> {});
-		DimensionRenderingRegistry.registerCloudRenderer(SKIES, ctx -> {});
-		DimensionRenderingRegistry.registerWeatherRenderer(DREAMSCAPE, ctx -> {});
-		DimensionRenderingRegistry.registerWeatherRenderer(SKIES, ctx -> {});
+		IEventBus modBus = ModList.get().getModContainerById("cerulean").orElseThrow().getEventBus();
+
+		modBus.addListener(RegisterDimensionSpecialEffectsEvent.class, event -> {
+			event.register(DREAMSCAPE, new DimensionSpecialEffects(Float.NaN, false, DimensionSpecialEffects.SkyType.NORMAL, false, false) {
+				@Override
+				public Vec3 getBrightnessDependentFogColor(Vec3 fogColor, float brightness) {
+					return fogColor;
+				}
+
+				@Override
+				public boolean isFoggyAt(int x, int y) {
+					return false;
+				}
+
+				@Override
+				public boolean renderClouds(ClientLevel level, int ticks, float partialTick, PoseStack poseStack, double camX, double camY, double camZ, Matrix4f modelViewMatrix, Matrix4f projectionMatrix) {
+					return true;
+				}
+
+				@Override
+				public boolean renderSnowAndRain(ClientLevel level, int ticks, float partialTick, LightTexture lightTexture, double camX, double camY, double camZ) {
+					return true;
+				}
+
+				@Override
+				public boolean tickRain(ClientLevel level, int ticks, Camera camera) {
+					return true;
+				}
+			});
+			event.register(SKIES, new DimensionSpecialEffects(Float.NaN, false, DimensionSpecialEffects.SkyType.NORMAL, true, false) {
+				@Override
+				public Vec3 getBrightnessDependentFogColor(Vec3 fogColor, float brightness) {
+					return fogColor;
+				}
+
+				@Override
+				public boolean isFoggyAt(int x, int y) {
+					return false;
+				}
+
+				@Override
+				public boolean renderClouds(ClientLevel level, int ticks, float partialTick, PoseStack poseStack, double camX, double camY, double camZ, Matrix4f modelViewMatrix, Matrix4f projectionMatrix) {
+					return true;
+				}
+
+				@Override
+				public boolean renderSnowAndRain(ClientLevel level, int ticks, float partialTick, LightTexture lightTexture, double camX, double camY, double camZ) {
+					return true;
+				}
+
+				@Override
+				public boolean tickRain(ClientLevel level, int ticks, Camera camera) {
+					return true;
+				}
+			});
+		});
 	}
 }
