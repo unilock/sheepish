990b82cd56e6766e529bd96a20fc605550458433
unilock <unilock@fennet.rentals>
2025-08-04 12:52:51 -0400

    fix coremod
    doesn't do anything as annotations cannot be removed during runtime(?)

--- sheepish-asm/src/main/java/cc/unilock/sheepish/asm/SheepishCoreMod.java
+++ sheepish-asm/src/main/java/cc/unilock/sheepish/asm/SheepishCoreMod.java
@@ -19,39 +19,36 @@ public class SheepishCoreMod implements ICoreMod {
 
 	@Override
 	public Iterable<? extends ITransformer<?>> getTransformers() {
-		LOGGER.info("SheepishCoreMod");
-
-		return Collections.singleton(
-				new ITransformer<ClassNode>() {
-					@Override
-					public @NotNull ClassNode transform(ClassNode input, ITransformerVotingContext context) {
-						for (var annotation : input.visibleAnnotations) {
-							LOGGER.info("visibleAnnotations: "+annotation.toString());
-						}
-						for (var annotation : input.invisibleAnnotations) {
-							LOGGER.info("invisibleAnnotations: "+annotation.toString());
-						}
-						return input;
-					}
-
-					@Override
-					public @NotNull TransformerVoteResult castVote(ITransformerVotingContext context) {
-						return TransformerVoteResult.YES;
-					}
-
-					@Override
-					public @NotNull Set<Target<ClassNode>> targets() {
-						return ImmutableSet.of(
-								Target.targetClass("vectorwing.farmersdelight.integration.jei.JEIPlugin"),
-								Target.targetClass("de.ellpeck.actuallyadditions.mod.jei.JEIActuallyAdditionsPlugin")
-						);
-					}
-
-					@Override
-					public @NotNull TargetType<ClassNode> getTargetType() {
-						return TargetType.CLASS;
-					}
-				}
-		);
+		return Collections.emptyList();
+//		return Collections.singleton(
+//				new ITransformer<ClassNode>() {
+//					@Override
+//					public @NotNull ClassNode transform(ClassNode input, ITransformerVotingContext context) {
+//						LOGGER.info("Removing @JeiPlugin from "+context.getClassName());
+//
+//						input.invisibleAnnotations = Collections.emptyList();
+//
+//						return input;
+//					}
+//
+//					@Override
+//					public @NotNull TransformerVoteResult castVote(ITransformerVotingContext context) {
+//						return TransformerVoteResult.YES;
+//					}
+//
+//					@Override
+//					public @NotNull Set<Target<ClassNode>> targets() {
+//						return ImmutableSet.of(
+//								Target.targetClass("vectorwing.farmersdelight.integration.jei.JEIPlugin"),
+//								Target.targetClass("de.ellpeck.actuallyadditions.mod.jei.JEIActuallyAdditionsPlugin")
+//						);
+//					}
+//
+//					@Override
+//					public @NotNull TargetType<ClassNode> getTargetType() {
+//						return TargetType.CLASS;
+//					}
+//				}
+//		);
 	}
 }
