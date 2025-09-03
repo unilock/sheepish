6e7db612dde686c37a3e0dc228c12a8997167dfd
unilock <katherines1337@outlook.com>
2025-09-03 19:55:52 -0400

    tweak yumi fix

--- src/main/java/cc/unilock/sheepish/mixin/yumi_mc_core/YumiFoundationModMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/yumi_mc_core/YumiFoundationModMixin.java
@@ -1,28 +1,32 @@
 package cc.unilock.sheepish.mixin.yumi_mc_core;
 
-import cc.unilock.sheepish.Sheepish;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.yumi.mc.core.api.ModContainer;
 import dev.yumi.mc.core.impl.YumiFoundationMod;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Collection;
 
 @Mixin(value = YumiFoundationMod.class, remap = false)
 public class YumiFoundationModMixin {
+	@Shadow @Final private static Logger LOGGER;
+
 	/**
 	 * @author unilock
-	 * @reason catch StackOverflowError and fail soft
+	 * @reason avoid StackOverflowError and fail soft
 	 */
 	@WrapOperation(method = "populateMods", at = @At(value = "INVOKE", target = "Ldev/yumi/mc/core/impl/YumiFoundationMod;populateMods(Ljava/lang/StringBuilder;ILjava/util/Collection;)V"))
 	private static void populateMods$populateMods(StringBuilder contained, int depth, Collection<ModContainer> mods, Operation<Void> original, @Local ModContainer mod) {
-		try {
+		if (depth < 9) {
 			original.call(contained, depth, mods);
-		} catch (StackOverflowError e) {
-			Sheepish.LOGGER.error("Error parsing "+mod.getName()+"'s contained mods!");
+		} else {
+			LOGGER.error("Error parsing {}'s contained mods!", mod.getName());
 		}
 	}
 }
