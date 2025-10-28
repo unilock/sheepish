a3b91bea2bfef720115625a1a5f0c7cfc17ea6b3
unilock <katherines1337@outlook.com>
2025-10-28 07:05:37 -0400

    workaround for Infinity Cave bug...

--- src/main/java/cc/unilock/sheepish/mixin/minecraft/PlayerAdvancementsMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/minecraft/PlayerAdvancementsMixin.java
@@ -1,17 +1,22 @@
 package cc.unilock.sheepish.mixin.minecraft;
 
 import cc.unilock.sheepish.Sheepish;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.server.PlayerAdvancements;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerAdvancements.class)
 public class PlayerAdvancementsMixin {
-	@Inject(method = "award", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/AdvancementRewards;grant(Lnet/minecraft/server/level/ServerPlayer;)V"))
-	private static void award$grant(AdvancementHolder advancement, String criterionKey, CallbackInfoReturnable<Boolean> cir) {
-		Sheepish.LOGGER.debug("Attempting to grant advancement \"{}\". criterion \"{}\"", advancement.id(), criterionKey);
+//	@WrapOperation(method = "award", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/AdvancementRewards;grant(Lnet/minecraft/server/level/ServerPlayer;)V"))
+	@WrapMethod(method = "award")
+	private static boolean award$wrap(AdvancementHolder advancement, String criterionKey, Operation<Boolean> original) {
+		try {
+			return original.call(advancement, criterionKey);
+		} catch (AbstractMethodError ignored) {
+			Sheepish.LOGGER.error("Failed to grant advancement \"{}\", criterion \"{}\"", advancement.id(), criterionKey);
+			return false;
+		}
 	}
 }
