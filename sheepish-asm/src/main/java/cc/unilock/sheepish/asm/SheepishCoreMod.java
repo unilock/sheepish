c63a70a413e03a1dd020229475faed243bf8ad77
unilock <katherines1337@outlook.com>
2025-11-15 15:19:08 -0500

    java hates me...

--- sheepish-asm/src/main/java/cc/unilock/sheepish/asm/SheepishCoreMod.java
+++ sheepish-asm/src/main/java/cc/unilock/sheepish/asm/SheepishCoreMod.java
@@ -8,7 +8,8 @@ import cpw.mods.modlauncher.api.TargetType;
 import cpw.mods.modlauncher.api.TransformerVoteResult;
 import net.neoforged.neoforgespi.coremod.ICoreMod;
 import org.jetbrains.annotations.NotNull;
-import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
 import org.slf4j.Logger;
 
 import java.util.Collections;
@@ -21,32 +22,38 @@ public class SheepishCoreMod implements ICoreMod {
 	public Iterable<? extends ITransformer<?>> getTransformers() {
 		return Collections.emptyList();
 //		return Collections.singleton(
-//				new ITransformer<ClassNode>() {
+//				new ITransformer<MethodNode>() {
 //					@Override
-//					public @NotNull ClassNode transform(ClassNode input, ITransformerVotingContext context) {
-//						LOGGER.info("Removing @JeiPlugin from "+context.getClassName());
+//					public @NotNull MethodNode transform(MethodNode methodNode, ITransformerVotingContext context) {
+//						LOGGER.info("Transforming java/awt/Toolkit.initStatic()V");
 //
-//						input.invisibleAnnotations = Collections.emptyList();
+//						var insns = new InsnList();
+//						insns.add(new TypeInsnNode(Opcodes.NEW, "java/lang/RuntimeException"));
+//						insns.add(new InsnNode(Opcodes.DUP));
+//						insns.add(new LdcInsnNode("Toolkit!"));
+//						insns.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, "java/lang/RuntimeException", "<init>", "(Ljava/lang/String;)V"));
+//						insns.add(new InsnNode(Opcodes.ATHROW));
 //
-//						return input;
+//						methodNode.instructions.insert(insns);
+//
+//						return methodNode;
 //					}
 //
 //					@Override
-//					public @NotNull TransformerVoteResult castVote(ITransformerVotingContext context) {
+//					public @NotNull TransformerVoteResult castVote(ITransformerVotingContext iTransformerVotingContext) {
 //						return TransformerVoteResult.YES;
 //					}
 //
 //					@Override
-//					public @NotNull Set<Target<ClassNode>> targets() {
+//					public @NotNull Set<Target<MethodNode>> targets() {
 //						return ImmutableSet.of(
-//								Target.targetClass("vectorwing.farmersdelight.integration.jei.JEIPlugin"),
-//								Target.targetClass("de.ellpeck.actuallyadditions.mod.jei.JEIActuallyAdditionsPlugin")
+//								Target.targetMethod("java.awt.Toolkit", "initStatic", "()V")
 //						);
 //					}
 //
 //					@Override
-//					public @NotNull TargetType<ClassNode> getTargetType() {
-//						return TargetType.CLASS;
+//					public @NotNull TargetType<MethodNode> getTargetType() {
+//						return TargetType.METHOD;
 //					}
 //				}
 //		);
