9c9da92eaba67c92b19eb7173909154f48880c04
unilock <katherines1337@outlook.com>
2026-02-09 13:51:30 -0500

    ignoredEnchantments: target EnchantRandomlyFunction instead of EnchantmentHelper

--- src/main/java/cc/unilock/sheepish/mixin/minecraft/EnchantRandomlyFunctionMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/minecraft/EnchantRandomlyFunctionMixin.java
@@ -1,8 +1,8 @@
 package cc.unilock.sheepish.mixin.minecraft;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -10,9 +10,9 @@ import java.util.List;
 
 import static cc.unilock.sheepish.SheepishConfig.CONFIG;
 
-@Mixin(EnchantmentHelper.class)
-public class EnchantmentHelperMixin {
-	@ModifyReturnValue(method = "selectEnchantment", at = @At(value = "RETURN"))
+@Mixin(EnchantRandomlyFunction.class)
+public class EnchantRandomlyFunctionMixin {
+	@ModifyExpressionValue(method = "run", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;toList()Ljava/util/List;"))
 	private static List<EnchantmentInstance> filterEnchants(List<EnchantmentInstance> original) {
 		original.removeIf(instance -> CONFIG.ignoredEnchantments.value().contains(instance.enchantment.getRegisteredName()));
 		return original;
