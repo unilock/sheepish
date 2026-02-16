c6507c82857705a5db168ee63bf2e943e636b223
unilock <katherines1337@outlook.com>
2026-02-16 10:19:03 -0500

    fix again

--- src/main/java/cc/unilock/sheepish/mixin/minecraft/EnchantRandomlyFunctionMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/minecraft/EnchantRandomlyFunctionMixin.java
@@ -2,7 +2,8 @@ package cc.unilock.sheepish.mixin.minecraft;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.core.Holder;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +16,7 @@ import static cc.unilock.sheepish.SheepishConfig.CONFIG;
 @Mixin(EnchantRandomlyFunction.class)
 public class EnchantRandomlyFunctionMixin {
 	@WrapOperation(method = "run", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;toList()Ljava/util/List;"))
-	private static List<EnchantmentInstance> filterEnchants(Stream<EnchantmentInstance> stream, Operation<List<EnchantmentInstance>> original) {
-		return original.call(stream.filter(instance -> !CONFIG.ignoredEnchantments.value().contains(instance.enchantment.getRegisteredName())));
+	private static List<Holder<Enchantment>> filterEnchants(Stream<Holder<Enchantment>> stream, Operation<List<Holder<Enchantment>>> original) {
+		return original.call(stream.filter(instance -> !CONFIG.ignoredEnchantments.value().contains(instance.getRegisteredName())));
 	}
 }
