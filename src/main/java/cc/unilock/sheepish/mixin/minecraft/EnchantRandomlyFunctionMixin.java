8731afeb99781da5c42cf4ea327e234a9bf05955
unilock <katherines1337@outlook.com>
2026-02-16 06:37:25 -0500

    fix UnsupportedOperationException in EnchantRandomlyFunctionMixin

--- src/main/java/cc/unilock/sheepish/mixin/minecraft/EnchantRandomlyFunctionMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/minecraft/EnchantRandomlyFunctionMixin.java
@@ -1,20 +1,21 @@
 package cc.unilock.sheepish.mixin.minecraft;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static cc.unilock.sheepish.SheepishConfig.CONFIG;
 
 @Mixin(EnchantRandomlyFunction.class)
 public class EnchantRandomlyFunctionMixin {
-	@ModifyExpressionValue(method = "run", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;toList()Ljava/util/List;"))
-	private static List<EnchantmentInstance> filterEnchants(List<EnchantmentInstance> original) {
-		original.removeIf(instance -> CONFIG.ignoredEnchantments.value().contains(instance.enchantment.getRegisteredName()));
-		return original;
+	@WrapOperation(method = "run", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;toList()Ljava/util/List;"))
+	private static List<EnchantmentInstance> filterEnchants(Stream<EnchantmentInstance> stream, Operation<List<EnchantmentInstance>> original) {
+		return original.call(stream.filter(instance -> !CONFIG.ignoredEnchantments.value().contains(instance.enchantment.getRegisteredName())));
 	}
 }
