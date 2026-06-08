986393be7fa6536523bd163ab79ef7a5de40f33d
unilock <katherines1337@outlook.com>
2026-06-08 08:24:50 -0400

    also ignore composter provider registration

--- src/main/java/cc/unilock/sheepish/mixin/fabric_transfer_api_v1/TransferApiNeoCompatMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/fabric_transfer_api_v1/TransferApiNeoCompatMixin.java
@@ -4,7 +4,9 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import org.sinytra.fabric.transfer_api.TransferApiNeoCompat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(value = TransferApiNeoCompat.class, remap = false)
 public class TransferApiNeoCompatMixin {
 	@ModifyExpressionValue(method = "lambda$wrapProviderSafely$10", at = @At(value = "INVOKE", target = "Ljava/lang/ThreadLocal;get()Ljava/lang/Object;"))
-	private static Object wrapProviderSafely(Object original, @Local(argsOnly = true) BlockEntity blockEntity) {
+	private static Object wrapProviderSafely(Object original, @Local(argsOnly = true) BlockState state, @Local(argsOnly = true) BlockEntity blockEntity) {
 		String namespace = null;
 		if (blockEntity != null) {
 			ResourceLocation rl = BuiltInRegistries.BLOCK_ENTITY_TYPE.getKey(blockEntity.getType());
@@ -20,6 +22,6 @@ public class TransferApiNeoCompatMixin {
 				namespace = rl.getNamespace();
 			}
 		}
-		return (Boolean) original && "minecraft".equals(namespace);
+		return (Boolean) original && (state.is(Blocks.COMPOSTER) || "minecraft".equals(namespace));
 	}
 }
