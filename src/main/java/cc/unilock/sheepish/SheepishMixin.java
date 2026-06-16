b831fbef4f3b533a049bb4db594e8bd70d5134e6
unilock <katherines1337@outlook.com>
2026-06-16 18:18:31 -0400

    only fix newer trickster versions

--- src/main/java/cc/unilock/sheepish/SheepishMixin.java
+++ src/main/java/cc/unilock/sheepish/SheepishMixin.java
@@ -3,6 +3,7 @@ package cc.unilock.sheepish;
 import com.bawnorton.mixinsquared.adjuster.MixinAnnotationAdjusterRegistrar;
 import com.bawnorton.mixinsquared.canceller.MixinCancellerRegistrar;
 import net.neoforged.fml.loading.LoadingModList;
+import net.neoforged.fml.loading.moddiscovery.ModFileInfo;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -27,15 +28,21 @@ public class SheepishMixin implements IMixinConfigPlugin {
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
 		String id = mixinClassName.split("\\.")[4];
+		ModFileInfo modFile = LoadingModList.get().getModFileById(id);
 
 		if ("emi".equals(id)) {
-			return LoadingModList.get().getModFileById(id) != null && CONFIG.emiHacks.value();
+			return modFile != null && CONFIG.emiHacks.value();
 		}
 		if ("fabric_transfer_api_v1".equals(id)) {
-			return LoadingModList.get().getModFileById(id) != null && CONFIG.fabricTransferApiHacks.value();
+			return modFile != null && CONFIG.fabricTransferApiHacks.value();
+		}
+		if ("trickster".equals(id)) {
+			// Trickster 2.0.0-beta.34 is the latest version that works without our fixes
+			// Don't bother accounting for older versions. I'm not JiJ'ing FlexVer
+			return modFile != null && !modFile.versionString().equals("2.0.0-beta.34");
 		}
 
-		return LoadingModList.get().getModFileById(id) != null;
+		return modFile != null;
 	}
 
 	@Override
