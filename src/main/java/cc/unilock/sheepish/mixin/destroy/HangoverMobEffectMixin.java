cd0c46d5d46eb97ec87aa192e6148c1436b695c4
unilock <katherines1337@outlook.com>
2026-08-13 11:57:55 -0400

    pseudo

--- src/main/java/cc/unilock/sheepish/mixin/destroy/HangoverMobEffectMixin.java
+++ src/main/java/cc/unilock/sheepish/mixin/destroy/HangoverMobEffectMixin.java
@@ -5,8 +5,10 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.server.level.ServerLevel;
 import net.neoforged.neoforge.event.PlayLevelSoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 
 @Mixin(targets = "petrolpark/mc/destroy/content/product/alcohol/HangoverMobEffect")
+@Pseudo
 public class HangoverMobEffectMixin {
 	@WrapMethod(method = "onPlayerHearsSound")
 	private static void onPlayerHearsSound(PlayLevelSoundEvent.AtPosition event, Operation<Void> original) {
