7a4b17c4711c5e58209dda3ecee51200fbe2cc1e
unilock <katherines1337@outlook.com>
2026-08-13 11:56:48 -0400

    attempt to fix Destroy causing CMEs

---
+++ src/main/java/cc/unilock/sheepish/mixin/destroy/HangoverMobEffectMixin.java
@@ -0,0 +1,17 @@
+package cc.unilock.sheepish.mixin.destroy;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.server.level.ServerLevel;
+import net.neoforged.neoforge.event.PlayLevelSoundEvent;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(targets = "petrolpark/mc/destroy/content/product/alcohol/HangoverMobEffect")
+public class HangoverMobEffectMixin {
+	@WrapMethod(method = "onPlayerHearsSound")
+	private static void onPlayerHearsSound(PlayLevelSoundEvent.AtPosition event, Operation<Void> original) {
+		if (event.getLevel() instanceof ServerLevel serverLevel) {
+			serverLevel.getServer().execute(() -> original.call(event));
+		}
+	}
+}
